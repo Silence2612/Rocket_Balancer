024e4c8c0ff7a54f31d63f3b3894c249d7097545
Vanshit Shivani <vanshitshivani666@gmail.com>
2025-02-10 17:20:55 +0530

    level changes

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -12,16 +12,25 @@ public class CollisionHandler : MonoBehaviour
              Debug.Log("Start");
              break;
              case "Finish":
+             Invoke("NextLevel" , 2.0f);
              Debug.Log("Finished");
              break;
              default:
              Debug.Log("Explode");
-             ReloadLevel();
+             Invoke("ReloadLevel" , 2.0f);
              break;
         }
     }
     void ReloadLevel(){
         int CurrentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(CurrentScene);
+    }
+
+    void NextLevel(){
+        int CurrentScene = SceneManager.GetActiveScene().buildIndex;
+        if(CurrentScene == SceneManager.sceneCountInBuildSettings - 1){
+            CurrentScene = -1;
+        }
+            SceneManager.LoadScene(CurrentScene+1);
     }
 }
